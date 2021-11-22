42f5ca1c2b689cb7d91bcabcbb1aec5999f522e8
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-22 20:07:58 +0000

    First attempt

---
+++ src/js/component/TrafficLights.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+//import PropTypes from "prop-types";
+
+// Resources
+import "../../styles/TrafficLights.scss";
+
+const TraficLights = () => {
+	//hooks
+	const [color, setColor] = useState("green");
+
+	return (
+		<div className="container">
+			<div
+				onClick={() => setColor("red")}
+				className={
+					color === "red" ? "red light selected" : "red light"
+				}></div>
+			<div
+				onClick={() => setColor("yellow")}
+				className={
+					color === "yellow"
+						? "yellow light selected"
+						: "yellow light"
+				}></div>
+			<div
+				onClick={() => setColor("green")}
+				className={
+					color === "green" ? "green light selected" : "green light"
+				}></div>
+		</div>
+	);
+};
+
+export default TraficLights;
